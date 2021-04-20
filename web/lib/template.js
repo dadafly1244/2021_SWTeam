22fcdccccc0303e60fdd9810268ea8fc45e8fc0b
Yerin00 <zlekfl0311@naver.com>
2021-04-21 00:03:20 +0900

    web modified

--- web/lib/template.js
+++ web/lib/template.js
@@ -4,9 +4,46 @@ module.exports = {
     <!doctype html>
     <html>
     <head>
+      <style>
+        #grid{
+          display:grid;
+          grid-template-rows: 30px 1fr;
+        }
+        div {
+          text-align:center;
+        }
+        h1 {
+          font-size: 40px;
+          text-align: center;
+        }
+        a {
+          text-decoration-line: none;
+        }
+        .myButton {
+          width:240px;
+          background-color:#44c767;
+          border-radius:30px;
+          border:2px solid #18ab29;
+          display:inline-block;
+          cursor:pointer;
+          color:#ffffff;
+          font-size:28px;
+          font-weight:bold;
+          padding:32px 54px;
+          text-decoration:none;
+          text-shadow:0px 1px 0px #2f6627;
+          margin:10px;
+        }
+        .myButton:hover {
+          background-color:#5cbf2a;
+        }
+        .myButton:active {//when click button
+          position:relative;
+          top:1px;
+        }
+      </style>
       <title>STCat House - ${title}</title>
       <meta charset="utf-8">
-      <link rel="stylesheet" href="./css/main.css">
     </head>
     <body>
       <h1><a href="/">STCat House</a></h1>
@@ -27,3 +64,13 @@ module.exports = {
     return list;
   }
 }
+
+/*<link rel="stylesheet" href="/css/main.css">
+<script>
+  function sheetLoaded() {
+  // Do something interesting; the sheet has been loaded
+  }
+  function sheetError() {
+    alert("An error occurred loading the stylesheet!");
+  }
+</script>*/
--- web/main.js
+++ web/main.js
@@ -12,13 +12,21 @@ var app = http.createServer(function(request,response){
       if(queryData.id === undefined){//home화면.쿼리없는경우.
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}
-            <a href="/feed_process"><input type="button" value="사료주기"></a>
-            <a href="/map"><input type="button" value="지도"></a>`,
-            `<a href="/create">create</a>`
+          var description = 'Hello, this is application for stray cats!';
+          //<a href="#" class="myButton">사료주기</a> css적용안됨 why?
+          var html = template.HTML(title, '',
+            `<div style="background-size:400px 120px;padding-bottom:50px;background-color:lightblue;"><h2>${title}</h2>${description}</div>
+            <div id="grid">
+              <div id="row1">
+                <a href="/feed_process"><input type="button" class="myButton" value="사료주기"></a>
+                <a href="/water_process"><input type="button" class="myButton" value="물주기"></a>
+              </div>
+              <div id="row2">
+                <a href="/map"><input type="button" class="myButton" value="지도"></a>
+                <a href="/community"><input type="button" class="myButton" value="커뮤니티"></a>
+              </div>
+            </div>`,
+            ''
           );
           response.writeHead(200);
           response.end(html);
@@ -133,14 +141,39 @@ var app = http.createServer(function(request,response){
         response.writeHead(302, {Location: '/'});//홈화면으로 돌아가기?.
         response.end();
 
+    } else if(pathname === '/water_process'){//사료주기버튼 클릭시.
+        //IoT기기의 펌프모터 동작시키기.
+        response.writeHead(302, {Location: '/'});//홈화면으로 돌아가기?.
+        response.end();
+
     } else if(pathname === '/map'){
       fs.readdir('./data', function(error, filelist){
         var title = 'STCat House - Map';
-        var list = template.list(filelist);
         //body부분에 지도API를 본문으로 가져오기.
-        var html = template.HTML(title, list, `<h2>Map</h2>
+        var html = template.HTML(title,'',
+          `<h2>Map</h2>
+           <div id="map" style="width:500px;height:400px;"></div>
+  	       <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=c6ed5d810eeed58e0c7cadca05d82aad"></script>
+  	       <script>
+  		       var container = document.getElementById('map');
+  		       var options = {center: new kakao.maps.LatLng(35.888836,128.608111),level: 2};
+            var map = new kakao.maps.Map(container, options);
+  	       </script>`,
+          '');
+        response.writeHead(200);
+        response.end(html);
+      });
 
-        `, '');
+    } else if(pathname === '/community'){
+      fs.readdir('./data', function(error, filelist){
+        var title = 'Community';
+        var description = "Let's share your tips!";
+        var list = template.list(filelist);
+        //<a href="#" class="myButton">사료주기</a> css적용안됨 why?
+        var html = template.HTML(title, list,
+          `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`
+        );
         response.writeHead(200);
         response.end(html);
       });
